be3b34ad11257c24e877504fbbf9a92435984a5c
RomRav <romain.ravault@gmail.com>
2022-02-01 16:04:06 +0100

    Ajout de la configuration de la connection à la BD.

---
+++ agenda/Db/DbConnect.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace agenda.Db
+{
+    public class DbConnect : DbContext
+    {
+        public DbConnect(DbContextOptions<DbConnect> option) : base(option)
+        {
+
+        }
+
+        
+    }
+}
