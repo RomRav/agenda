28299c4500ba2b99c488e12e9a0795ef090de6a6
RomRav <romain.ravault@gmail.com>
2022-02-02 08:51:02 +0100

    Ajout des classes via EF à partir de la BD agenda.

---
+++ agenda/Controllers/AppointmentController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace agenda.Controllers
+{
+    public class AppointmentController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ agenda/Models/Broker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace agenda.Models
+{
+    public partial class Broker
+    {
+        public Broker()
+        {
+            Appointments = new HashSet<Appointment>();
+        }
+
+        public int IdBroker { get; set; }
+        public string Lastname { get; set; } = null!;
+        public string Firstname { get; set; } = null!;
+        public string Mail { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+
+        public virtual ICollection<Appointment> Appointments { get; set; }
+    }
+}
